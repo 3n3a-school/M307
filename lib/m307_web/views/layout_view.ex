200221eed9ac0e17e3b374086061d4db0029f5c2
3n3a <info@3n3a.ch>
2022-05-05 09:33:05 +0200

    add company branding

--- lib/m307_web/views/layout_view.ex
+++ lib/m307_web/views/layout_view.ex
@@ -1,6 +1,10 @@
 defmodule M307Web.LayoutView do
   use M307Web, :view
 
+  def company_name() do
+    "Kredihay"
+  end
+
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
