93bff717f6c2e3bfc5af72609ee2174b0ec8bb6f
Kris Huber <internet@krishuber.xyz>
2022-05-10 11:14:50 +0200

    Loan Deadline Logic

--- lib/m307_web/views/loan_view.ex
+++ lib/m307_web/views/loan_view.ex
@@ -2,8 +2,8 @@ defmodule M307Web.LoanView do
   use M307Web, :view
 
   def loan_deadline(loan) do
-    if Date.diff(loan.inserted_at, Date.utc_today()) > loan.rate_count * 15 ||
-         loan.rate_count > 10 do
+    if Date.diff(loan.inserted_at, Date.utc_today()) > loan.rate_count * 15 and
+         loan.status == :open do
       "⚡"
     else
       "🌞"
