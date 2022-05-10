5f50f8b1283fe9baa3ce99c082b1a89185a19632
3n3a <info@3n3a.ch>
2022-05-10 16:26:16 +0200

    translation to english

--- lib/m307/credit/loan.ex
+++ lib/m307/credit/loan.ex
@@ -17,8 +17,8 @@ defmodule M307.Credit.Loan do
   def changeset(loan, attrs) do
     loan
     |> cast(attrs, [:name, :email, :phone, :rate_count, :status, :credit_package])
-    |> validate_required([:name, :email, :phone, :rate_count, :status, :credit_package],
-      message: "Dieses Feld ist ein Pflichtfeld."
+    |> validate_required([:name, :email, :rate_count, :status, :credit_package],
+      message: "This field is required, please fill it out."
     )
     |> validate_email()
     |> validate_phone()
@@ -27,8 +27,8 @@ defmodule M307.Credit.Loan do
 
   defp validate_email(changeset) do
     changeset
-    |> validate_format(:email, ~r/@/, message: "Email muss ein '@' Zeichen enthalten.")
-    |> unique_constraint(:email, message: "Diese Email Adresse existiert bereits.")
+    |> validate_format(:email, ~r/@/, message: "Email must include an '@' sign.")
+    |> unique_constraint(:email, message: "This email address already exists.")
   end
 
   defp validate_phone(changeset) do
@@ -36,16 +36,16 @@ defmodule M307.Credit.Loan do
     |> validate_format(
       :phone,
       ~r/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/i,
-      message: "Die eingegebene Telefonnummer hat ein ungültiges Format."
+      message: "The entered phonenumber exists in a wrong format."
     )
   end
 
   def validate_rate_count(changeset) do
     changeset
     |> validate_number(:rate_count,
-      greater_than_or_equal_to: 0,
+      greater_than_or_equal_to: 1,
       less_than_or_equal_to: 10,
-      message: "Die Anzahl Raten muss zwischen null und zehn liegen."
+      message: "The number of rates has to be between 1 and 10."
     )
   end
 end
