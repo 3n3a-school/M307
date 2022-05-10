7d0c9dff488766aabe97f659bfe0dc799f356cc1
3n3a <info@3n3a.ch>
2022-05-10 14:07:27 +0200

    add functions

--- lib/m307_web/views/loan_view.ex
+++ lib/m307_web/views/loan_view.ex
@@ -1,12 +1,23 @@
 defmodule M307Web.LoanView do
   use M307Web, :view
 
-  def loan_deadline(loan) do
-    if Date.diff(loan.inserted_at, Date.utc_today()) > loan.rate_count * 15 and
+  alias M307.Credit
+
+  def deadline_status(loan) do
+    if Date.diff(Date.utc_today(), loan.inserted_at) > loan.rate_count * 15 and
          loan.status == :open do
       "⚡"
     else
       "🌞"
     end
+  end
+
+  def days_until_deadline(loan) do
+    IO.puts Date.diff(loan.inserted_at, Date.utc_today())
+    (loan.rate_count * 15) - Date.diff(Date.utc_today(), loan.inserted_at)
+  end
+
+  def get_credit_package(loan) do
+    Credit.get_package!(loan.credit_package).name
   end
 end
