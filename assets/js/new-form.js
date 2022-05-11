a6f850b272a3d50b9f800e1b3bad8a917f7b445e
3n3a <info@3n3a.ch>
2022-05-11 12:00:23 +0200

    add form calculation

--- assets/js/new-form.js
+++ assets/js/new-form.js
@@ -1,12 +1,26 @@
-if (location.pathname.split("/").reverse()[0] == "new") {
-    console.log("is new page")
-    newPageValidator()
+try {
+    if (document.getElementById('new-form-exists')) {
+        newPageValidator()
+    }
+} catch (error) {
+    console.log("")
 }
 
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
 
 function updateRatePaypackDate(event) {
-    let rate_count_value = event.target.rate_count_value
-    console.log(rate_count_value)
+    let today = new Date()
+    let rate_count_value = Number(event.target.value) * 15
+    let payback_date = today.addDays(rate_count_value)
+    let payback_string = payback_date.toLocaleDateString("en-US", {timeZone: "UTC", weekday: "long", year: "numeric", month: "numeric", day: "numeric"})
+    let loan_rate_date = document.getElementById('loan_rate_payback_date')
+
+    loan_rate_date.parentElement.parentElement.hidden = false
+    loan_rate_date.innerHTML = `Pay loan back by ${payback_string}`
 }
 
 function newPageValidator() {
