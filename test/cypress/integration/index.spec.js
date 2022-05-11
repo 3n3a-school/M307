66c6ddd2dfab88afdae31e27e8344a9369bfd050
Lian Studer <ln.studer@protonmail.ch>
2022-05-11 13:40:37 +0200

    add edit loan cypress test

--- test/cypress/integration/index.spec.js
+++ test/cypress/integration/index.spec.js
@@ -14,14 +14,19 @@ describe('Integration Test', () => {
         cy.get("#loan_phone").type("+411234202030")
         cy.get("#loan_rate_count").type("7")
         cy.get("#loan_credit_package").select("8")
-        // const today = new Date();
-        // const paybackDate = new Date(
-        //     today.getFullYear(), today.getMonth(), 
-        //     today.getDay() + (cy.get("#loan_rate_count").value * 15))
-    
-        // cy.get("#loan_rate_payback_date").should("have.value", paybackDate)
+        cy.get("body > div > main > form > div").should('be.visible')
         cy.get("button[type=submit]").click()
         cy.visit(URL)
         cy.get("body > div > main > table > tbody").children().should("have.length", 1)
+    })
+
+    it('should edit a loan', () => {
+        cy.visit(URL)
+
+        cy.get("body > div > main > table > tbody > tr > td:nth-child(8) > span:nth-child(2) > a").click()
+        cy.get("#loan_email").clear().type("max.muster@suva.ch")
+        cy.get("button[type=submit]").click()
+        cy.visit(URL)
+        cy.get("body > div > main > table > tbody > tr > td:nth-child(2)").should("have.text", "max.muster@suva.ch")
     })
 })
